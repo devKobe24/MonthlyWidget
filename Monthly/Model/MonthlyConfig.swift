1f5dada9ec40e193239078451b1024a89d39f92d
Kobe <dev.skyachieve91@gmail.com>
2023-10-27 13:47:49 +0900

    :bug: Chore: july로 되어있던 june의 날짜, 요일 변경.

--- Monthly/Model/MonthlyConfig.swift
+++ Monthly/Model/MonthlyConfig.swift
@@ -19,84 +19,84 @@ struct MonthlyConfig {
         switch month {
         case 1:
             return MonthlyConfig(
-                backgroundColor: .gray,
+                backgroundColor: MonthlyContents.january.backgroundColor,
                 emojiText: MonthlyContents.january.emoji,
                 weekdayTextColor: MonthlyContents.january.weekdayTextColor,
                 dayTextColor: MonthlyContents.january.dayTextColor
             )
         case 2:
             return MonthlyConfig(
-                backgroundColor: .palePink,
+                backgroundColor: MonthlyContents.febuary.backgroundColor,
                 emojiText: MonthlyContents.febuary.emoji,
                 weekdayTextColor: MonthlyContents.febuary.weekdayTextColor,
                 dayTextColor: MonthlyContents.febuary.dayTextColor
             )
         case 3:
             return MonthlyConfig(
-                backgroundColor: .paleGreen,
+                backgroundColor: MonthlyContents.march.backgroundColor,
                 emojiText: MonthlyContents.march.emoji,
                 weekdayTextColor: MonthlyContents.march.weekdayTextColor,
                 dayTextColor: MonthlyContents.march.dayTextColor
             )
         case 4:
             return MonthlyConfig(
-                backgroundColor: .paleBlue,
+                backgroundColor: MonthlyContents.april.backgroundColor,
                 emojiText: MonthlyContents.april.emoji,
                 weekdayTextColor: MonthlyContents.april.weekdayTextColor,
                 dayTextColor: MonthlyContents.april.dayTextColor
             )
         case 5:
             return MonthlyConfig(
-                backgroundColor: .paleYellow,
+                backgroundColor: MonthlyContents.may.backgroundColor,
                 emojiText: MonthlyContents.may.emoji,
                 weekdayTextColor: MonthlyContents.may.weekdayTextColor,
                 dayTextColor: MonthlyContents.may.dayTextColor
             )
         case 6:
             return MonthlyConfig(
-                backgroundColor: .skyBlue,
+                backgroundColor: MonthlyContents.june.backgroundColor,
                 emojiText: MonthlyContents.june.emoji,
-                weekdayTextColor: MonthlyContents.july.weekdayTextColor,
-                dayTextColor: MonthlyContents.july.dayTextColor
+                weekdayTextColor: MonthlyContents.june.weekdayTextColor,
+                dayTextColor: MonthlyContents.june.dayTextColor
             )
         case 7:
             return MonthlyConfig(
-                backgroundColor: .blue,
+                backgroundColor: MonthlyContents.july.backgroundColor,
                 emojiText: MonthlyContents.july.emoji,
                 weekdayTextColor: MonthlyContents.july.weekdayTextColor,
                 dayTextColor: MonthlyContents.july.dayTextColor
             )
         case 8:
             return MonthlyConfig(
-                backgroundColor: .paleOrange,
+                backgroundColor: MonthlyContents.august.backgroundColor,
                 emojiText: MonthlyContents.august.emoji,
                 weekdayTextColor: MonthlyContents.august.weekdayTextColor,
                 dayTextColor: MonthlyContents.august.dayTextColor
             )
         case 9:
             return MonthlyConfig(
-                backgroundColor: .paleRed,
+                backgroundColor: MonthlyContents.september.backgroundColor,
                 emojiText: MonthlyContents.september.emoji,
                 weekdayTextColor: MonthlyContents.september.weekdayTextColor,
                 dayTextColor: MonthlyContents.september.dayTextColor
             )
         case 10:
             return MonthlyConfig(
-                backgroundColor: .black,
+                backgroundColor: MonthlyContents.october.backgroundColor,
                 emojiText: MonthlyContents.october.emoji,
                 weekdayTextColor: MonthlyContents.october.weekdayTextColor,
                 dayTextColor: MonthlyContents.october.dayTextColor
             )
         case 11:
             return MonthlyConfig(
-                backgroundColor: .paleBrown,
+                backgroundColor: MonthlyContents.november.backgroundColor,
                 emojiText: MonthlyContents.november.emoji,
                 weekdayTextColor: MonthlyContents.november.weekdayTextColor,
                 dayTextColor: MonthlyContents.november.dayTextColor
             )
         case 12:
             return MonthlyConfig(
-                backgroundColor: .paleRed,
+                backgroundColor: MonthlyContents.december.backgroundColor,
                 emojiText: MonthlyContents.december.emoji,
                 weekdayTextColor: MonthlyContents.december.weekdayTextColor,
                 dayTextColor: MonthlyContents.december.dayTextColor
